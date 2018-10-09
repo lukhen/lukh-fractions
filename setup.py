443aa24a0393b6291239aa037886cad0deb76dc2
lukhen <lukasz.hen@gmail.com>
2018-10-09 11:15:17 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Fractions module",
+    version="0.0",
+    author_email="lukasz.hen@gmail.com",
+    description="adding fractions",
+    py_modules=['fractions.py']
+)
