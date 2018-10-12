8fdde011e0f6bf0654e322d0287731927a4aa0e2
lukhen <lukasz.hen@gmail.com>
2018-10-12 05:35:13 +0200

    now 0 + fraction is possible

--- lukh_fractions.py
+++ lukh_fractions.py
@@ -1,9 +1,13 @@
 class LukhFraction:
-    def __init__(self, num):
+    def __init__(self, num, den=1):
         self._num = num
+        self._den = den
 
     def plus(self, other):
-        return LukhFraction(self._num + other._num)
+        return LukhFraction(self._num + other._num, other.getDen())
 
-    def getInt(self):
+    def getNum(self):
         return self._num
+
+    def getDen(self):
+        return self._den
--- tests/test_fractions.py
+++ tests/test_fractions.py
@@ -2,12 +2,17 @@ from lukh_fractions import LukhFraction
 
 
 def test_0_plus_0():
-    assert LukhFraction(0).plus(LukhFraction(0)).getInt() == 0
+    assert LukhFraction(0).plus(LukhFraction(0)).getNum() == 0
 
 
 def test_0_plus_NonZero():
-    assert LukhFraction(0).plus(LukhFraction(1)).getInt() == 1
+    assert LukhFraction(0).plus(LukhFraction(1)).getNum() == 1
 
 
 def test_NonZero_plus_NonZero():
-    assert LukhFraction(1).plus(LukhFraction(2)).getInt() == 3
+    assert LukhFraction(1).plus(LukhFraction(2)).getNum() == 3
+
+
+def test_Zero_plus_Fraction():
+    sum_result = LukhFraction(0).plus(LukhFraction(1, 2))
+    assert (sum_result.getNum(), sum_result.getDen()) == (1, 2)
