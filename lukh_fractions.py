1383ef7259914a73921bfe41a01e30b7b5966815
lukhen <lukasz.hen@gmail.com>
2018-10-15 06:24:38 +0200

    Fractions are reduced

--- lukh_fractions.py
+++ lukh_fractions.py
@@ -1,7 +1,8 @@
 class LukhFraction:
     def __init__(self, num, den=1):
-        self._num = num
-        self._den = den
+        gcd = find_gcd(*sorted([num, den]))
+        self._num = num / gcd
+        self._den = den / gcd
 
     def plus(self, other):
         if self._den == other.getDen():
@@ -16,3 +17,19 @@ class LukhFraction:
 
     def getDen(self):
         return self._den
+
+
+def find_gcd(x1, x2):
+    """
+    Find Greatest Common Denominator
+    x1, x2: integers
+    x1 >= x2
+    """
+    if x1 == 1 or x2 == 1:
+        return 1
+    else:
+        r = x1 % x2
+        if r == 0:
+            return x2
+        else:
+            return find_gcd(x2, r)
--- tests/test_fractions.py
+++ tests/test_fractions.py
@@ -25,9 +25,19 @@ def test_adding_fractions_with_common_denominator_1():
 
 def test_adding_fractions_with_common_denominator_2():
     sum_result = LukhFraction(1, 2).plus(LukhFraction(3, 2))
-    assert (sum_result.getNum(), sum_result.getDen()) == (4, 2)
+    assert (sum_result.getNum(), sum_result.getDen()) == (2, 1)
 
 
-def test_adding_fractions_no_common_denominator():
+def test_adding_fractions_no_common_denominator1():
     sum_result = LukhFraction(1, 2).plus(LukhFraction(1, 3))
     assert (sum_result.getNum(), sum_result.getDen()) == (5, 6)
+
+
+def test_adding_fractions_no_common_denominator2():
+    sum_result = LukhFraction(1, 6).plus(LukhFraction(1, 4))
+    assert (sum_result.getNum(), sum_result.getDen()) == (5, 12)
+
+
+def test_reduce_fraction():
+    f = LukhFraction(1701, 3768)
+    assert (f.getNum(), f.getDen()) == (567, 1256)
